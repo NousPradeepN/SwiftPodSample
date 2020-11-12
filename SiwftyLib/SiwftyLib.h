08ad1fd675dfb9294cde4c6e61d3fb402d5bfee4
Pradeep N <pradeepn@nousinfo.com>
2020-11-12 16:02:16 +0530

    first commit

---
+++ SiwftyLib/SiwftyLib.h
@@ -0,0 +1,18 @@
+//
+//  SiwftyLib.h
+//  SiwftyLib
+//
+//  Created by Pradeep on 11/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SiwftyLib.
+FOUNDATION_EXPORT double SiwftyLibVersionNumber;
+
+//! Project version string for SiwftyLib.
+FOUNDATION_EXPORT const unsigned char SiwftyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SiwftyLib/PublicHeader.h>
+
+
---
+++ SiwftyLib/SwiftyLib.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftyLib.swift
+//  SiwftyLib
+//
+//  Created by Pradeep on 11/11/20.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+    let name = "Swifty Lib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
